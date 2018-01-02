3e83ee5b2868bdff7b8a55c0144ea96bcb1d8979
Loren <lorenrose@icloud.com>
2018-01-01 20:25:32 -0600

    handlebars and node

---
+++ server.js
@@ -0,0 +1,65 @@
+var express = require("express");
+var app = express();
+var flash = require("connect-flash");
+
+var passport = require("passport");
+
+var session = require("express-session");
+var bodyParser = require("body-parser");
+
+var models = require("./models");
+app.use(express.static("Public"));
+
+var exphbs = require("express-handlebars");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.use(session({
+	secret:"oneringtorulethemall",
+	resave:true,
+	saveUninitialized:true
+}));
+app.use(passport.initialize());//initialize passport
+app.use(passport.session());
+
+var port = process.env.PORT || 8080;
+
+app.listen(port,function(err){
+	if(!err){
+		console.log("site is live");
+	} else {console.log(err);}
+})
+
+models.sequelize.sync().then(function(){
+	console.log("success");
+}).catch(function(err){
+	console.log(err,"failure");
+})
+
+app.engine("handlebars", 
+	exphbs({ 
+		defaultLayout: "main",
+		partialsDir:[__dirname+"/views/partials"]
+	 }));//make the main.handlebars be the layout template
+app.set("view engine","handlebars");//set the express view engine as handlebars
+
+//authentication, credentials and session instantiation
+require("./config/passport/passport.js")(passport,models.user,models.player,models.admin);
+
+//routes
+require("./routes/auth.js")(app,
+							passport,
+							models.user,
+							models.player,
+							models.admin);
+require("./routes/admin.js")(app,
+							models.player,
+							models.admin,
+							models.sequelize,
+							models.highscores);
+// require("./routes/game.js")(app,
+// 										);
+require("./routes/player.js")(app,
+								models.player,
+								models.game);
